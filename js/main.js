f127f1d6a85a5037aebfcc3908e2787cc9de6ed5
MarianaPacheco <pachecoramirez.mariana@gmail.com>
2016-10-18 17:26:19 -0500

    Prueba

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
 function validateForm(){
 	/* Escribe tú código aquí */
+	 var nombre = $("#name").val();	/*Mensaje nombre*/
+
+	if(nombre == ""){
+        $("#mensaje1").fadeIn("slow");
+        return false;
+    }
+    else{
+        $("#mensaje1").fadeOut();
+}
+
+
+
+if (nombre === "" || apellido === "" || correo === "" || contraseña === ""){	/*Si los campos están vacíos*/
+		$("#mensaje1").fadeIn("slow");
+		return false; 	/*Para que no avance si está vacío el campo*/	
+}  else{
+        $("#mensaje1").fadeOut();
 }
